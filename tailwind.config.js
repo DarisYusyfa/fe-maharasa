e87fd45a665b6b8545e908e0cf44f2e0eed9145d
DarisYusyfa <yusyfadaris@gmail.com>
2024-06-03 03:56:53 +0700

    update aos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+/* eslint-disable no-undef */
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
     extend: {
@@ -13,3 +13,4 @@ export default {
   },
   plugins: ['flowbite/plugin'],
 };
+/* eslint-disable no-undef */
